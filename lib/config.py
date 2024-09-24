6cf66f91d3ade381d659f578aaef3e52b58dc926
Cal Page <page.cal@gmail.com>
2024-09-24 06:45:37 -0400

    Refactor blacklist code and update IDE configurations
    Refactored function names and variable casing for consistency in `lib/blacklist.py`. Commented out unused imports to clean up the code. Enhanced `.idea` configuration files to include SDK details and auto-import settings.

--- lib/config.py
+++ lib/config.py
@@ -1,15 +1,23 @@
 import re
 import os
 import atexit
+import logging
+from logging import getLogger
+
+log_id = 'fail3ban'
+logger = logging.getLogger(log_id)
 
 class Config:
     def __init__(self, config_file='config.ctl'):
         self.config_data = {}
         self.load_config(config_file)
+        self.log_id = 'fail3ban'
+        self.logger = getLogger(log_id)
         # register a cleanup
         atexit.register(self.cleanup)
 
-    def get_ctl_path(self,control_file_name):
+    @staticmethod
+    def get_ctl_path(control_file_name):
         # Get the current working directory
         current_dir = os.getcwd()
         
@@ -31,9 +39,11 @@ class Config:
     def load_config(self, config_file):
         """Read the config.ctl file and store variable-value pairs."""
         # Open the config.ctl file
-        CONTROL_FILE = self.get_ctl_path(config_file)
+        control_file = Config.get_ctl_path(config_file)
+        if not control_file:
+            control_file = 'fail3ban'
         try:
-            with open(CONTROL_FILE, 'r') as file:
+            with open(control_file, 'r') as file:
                 for line in file:
                     # Strip whitespace and skip comments and empty lines
                     line = line.strip()
@@ -58,9 +68,9 @@ class Config:
                         self.config_data[variable] = value
 
         except FileNotFoundError:
-            print(f"Error: {CONTROL_FILE} not found.")
+            print(f"Error: {control_file} not found.")
         except Exception as e:
-            print(f"An error occurred while reading {CONTROL_FILE}: {e}")
+            print(f"An error occurred while reading {control_file}: {e}")
 
         # return this dictionary
         return self.config_data
