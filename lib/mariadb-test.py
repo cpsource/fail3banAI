e980427458e2e86086d3a764e6acb2f24af889d7
Cal Page <page.cal@gmail.com>
2024-10-04 21:33:45 +0000

    test mariadb

--- lib/mariadb-test.py
+++ lib/mariadb-test.py
@@ -1,14 +1,24 @@
 import mysql.connector
 import os
+from dotenv import load_dotenv
+
+# load dotenv
+try:
+    # Attempt to load dotenv file using the environment variable
+    dotenv_config = load_dotenv(f"{os.getenv('HOME')}/.env")
+    print("dotenv file loaded successfully.")
+except Exception as e:
+    # Handle any exceptions that may occur
+    print(f"An error occurred while loading dotenv: {e}")
 
 # Connect to MariaDB
 try:
     conn = mysql.connector.connect(
-        user="Ubuntu",
+        user=os.getenv('MARIADB_USER_NAME'),
         password=os.getenv('MARIADB_USER_PASSWORD'),
-        host="localhost",
+        host=os.getenv('MARIADB_USER_HOST'),
         port=3306,
-        database="fail3ban"
+        database=os.getenv('MARIADB_USER_DATABASE')
     )
 
     # Create a cursor object
